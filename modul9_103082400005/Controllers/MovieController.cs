babc07d34d92ad0c1500de98717856548e69cc33
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-04-28 11:55:30 +0700

    feat: menambahkan API Movies

---
+++ modul9_103082400005/Controllers/MovieController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul9_103082400005.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movieList = new List<Movie>
+        {
+            new Movie(
+                "The Shawshank Redemption",
+                "Frank Darabont",
+                new List<string> { "Tim Robbins", "Morgan Freeman", "Bob Gunton" },
+                "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+            ),
+
+            new Movie(
+                "The Godfather",
+                "Francis Ford Coppola",
+                new List<string> { "Marlon Brando", "Al Pacino", "James Caan" },
+                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+            ),
+
+            new Movie(
+                "The Dark Knight",
+                "Christopher Nolan",
+                new List<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart" },
+                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+            )
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return movieList;
+        }
+
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            return movieList[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie movieBaru)
+        {
+            movieList.Add(movieBaru);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= 0 && id < movieList.Count)
+            {
+                movieList.RemoveAt(id);
+            }
+        }
+    }
+}
--- modul9_103082400005/Program.cs
+++ modul9_103082400005/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +16,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +26,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+//app.MapGet("/", () => "Server API berhasil berjalan dan menerima request!");
 app.Run();
